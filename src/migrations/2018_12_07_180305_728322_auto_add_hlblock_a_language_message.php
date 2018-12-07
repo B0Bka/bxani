95704bc19617ba6bcf945fc33e6dd1a59b67eaa2
B0Bka <vovka.pasalsky@gmail.com>
2018-12-07 17:07:45 +0200

    create Lang highload

---
+++ src/migrations/2018_12_07_180305_728322_auto_add_hlblock_a_language_message.php
@@ -0,0 +1,40 @@
+<?php
+
+use Arrilot\BitrixMigrations\BaseMigrations\BitrixMigration;
+use Arrilot\BitrixMigrations\Exceptions\MigrationException;
+use Bitrix\Highloadblock\HighloadBlockTable;
+
+class AutoAddHlblockALanguageMessage20181207180305728322 extends BitrixMigration
+{
+    /**
+     * Run the migration.
+     *
+     * @return mixed
+     * @throws MigrationException
+     */
+    public function up()
+    {
+        $fields = array (
+  'NAME' => 'LanguageMessage',
+  'TABLE_NAME' => 'a_language_message',
+);
+
+        $result = HighloadBlockTable::add($fields);
+
+        if (!$result->isSuccess()) {
+            $errors = $result->getErrorMessages();
+            throw new MigrationException('Ошибка при добавлении hl-блока '.implode(', ', $errors));
+        }
+    }
+
+    /**
+     * Reverse the migration.
+     *
+     * @return mixed
+     * @throws MigrationException
+     */
+    public function down()
+    {
+        return false;
+    }
+}
---
+++ src/migrations/2018_12_07_180511_024819_auto_add_uf_uf_message_to_entity_hlblock_1.php
@@ -0,0 +1,74 @@
+<?php
+
+use Arrilot\BitrixMigrations\BaseMigrations\BitrixMigration;
+use Arrilot\BitrixMigrations\Exceptions\MigrationException;
+
+class AutoAddUfUfMessageToEntityHlblock120181207180511024819 extends BitrixMigration
+{
+    /**
+     * Run the migration.
+     *
+     * @return mixed
+     * @throws MigrationException
+     */
+    public function up()
+    {
+        $fields = array (
+  'ENTITY_ID' => 'HLBLOCK_1',
+  'FIELD_NAME' => 'UF_MESSAGE',
+  'USER_TYPE_ID' => 'string',
+  'XML_ID' => '',
+  'SORT' => '200',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'N',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 'a:6:{s:4:"SIZE";i:80;s:4:"ROWS";i:1;s:6:"REGEXP";s:0:"";s:10:"MIN_LENGTH";i:0;s:10:"MAX_LENGTH";i:0;s:13:"DEFAULT_VALUE";s:0:"";}',
+  'EDIT_FORM_LABEL' => 
+  array (
+    'ru' => 'Русский',
+    'en' => '',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'ru' => 'Русский',
+    'en' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'ru' => 'Русский',
+    'en' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'ru' => '',
+    'en' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'ru' => '',
+    'en' => '',
+  ),
+);
+
+        $this->addUF($fields);
+    }
+
+    /**
+     * Reverse the migration.
+     *
+     * @return mixed
+     * @throws MigrationException
+     */
+    public function down()
+    {
+        $id = $this->getUFIdByCode('HLBLOCK_1', 'UF_MESSAGE');
+        if (!$id) {
+            throw new MigrationException('Не найдено пользовательское свойство для удаления');
+        }
+
+        (new CUserTypeEntity())->delete($id);
+    }
+}
