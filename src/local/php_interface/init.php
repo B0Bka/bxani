4d3cd5b1f1b08433281a2d78ee1b589ef03a4476
B0Bka <vovka.pasalsky@gmail.com>
2018-12-07 16:51:33 +0200

    add composer and migrator

--- src/local/php_interface/init.php
+++ src/local/php_interface/init.php
@@ -1,3 +1,4 @@
 <?
 CModule::IncludeModule('aniart.main');
+Arrilot\BitrixMigrations\Autocreate\Manager::init($_SERVER["DOCUMENT_ROOT"].'/migrations');
 ?>
---
+++ src/migrations/2018_12_07_175037_481120_auto_add_uf_uf_item_to_entity_222.php
@@ -0,0 +1,74 @@
+<?php
+
+use Arrilot\BitrixMigrations\BaseMigrations\BitrixMigration;
+use Arrilot\BitrixMigrations\Exceptions\MigrationException;
+
+class AutoAddUfUfItemToEntity22220181207175037481120 extends BitrixMigration
+{
+    /**
+     * Run the migration.
+     *
+     * @return mixed
+     * @throws MigrationException
+     */
+    public function up()
+    {
+        $fields = array (
+  'ENTITY_ID' => '222',
+  'FIELD_NAME' => 'UF_ITEM',
+  'USER_TYPE_ID' => 'mail_message',
+  'XML_ID' => '',
+  'SORT' => '100',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'N',
+  'SHOW_FILTER' => 'N',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 'N;',
+  'EDIT_FORM_LABEL' => 
+  array (
+    'ru' => '',
+    'en' => '',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'ru' => '',
+    'en' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'ru' => '',
+    'en' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'ru' => '',
+    'en' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'ru' => '',
+    'en' => '',
+  ),
+);
+
+        $this->addUF($fields);
+    }
+
+    /**
+     * Reverse the migration.
+     *
+     * @return mixed
+     * @throws MigrationException
+     */
+    public function down()
+    {
+        $id = $this->getUFIdByCode('222', 'UF_ITEM');
+        if (!$id) {
+            throw new MigrationException('Не найдено пользовательское свойство для удаления');
+        }
+
+        (new CUserTypeEntity())->delete($id);
+    }
+}
